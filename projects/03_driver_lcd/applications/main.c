1da3ff9db07e3abf58ffe64aa66deb93298a0099
yangjie11 <yangjiexaut@126.com>
2023-09-25 17:10:56 +0800

    test

---
+++ projects/03_driver_lcd/applications/main.c
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2006-2021, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2023-5-10      ShiHao       first version
+ */
+
+#include <rtthread.h>
+#include <rtdevice.h>
+#include <board.h>
+
+#include <drv_lcd.h>
+#include <rttlogo.h>
+
+#define DBG_TAG "main"
+#define DBG_LVL         DBG_LOG
+#include <rtdbg.h>
+
+int main(void)
+{
+    lcd_clear(WHITE);
+
+    /* show RT-Thread logo */
+    lcd_show_image(0, 0, 240, 69, image_rttlogo);
+
+    /* set the background color and foreground color */
+    lcd_set_color(WHITE, BLACK);
+
+    /* show some string on lcd */
+    lcd_show_string(10, 69, 16, "Hello, RT-Thread!");
+    lcd_show_string(10, 69 + 16, 24, "RT-Thread");
+    lcd_show_string(10, 69 + 16 + 24, 32, "RT-Thread");
+
+    /* draw a line on lcd */
+    lcd_draw_line(0, 69 + 16 + 24 + 32, 240, 69 + 16 + 24 + 32);
+
+    /* draw a concentric circles */
+    lcd_draw_point(120, 194);
+    for (int i = 0; i < 46; i += 4)
+    {
+        lcd_draw_circle(120, 194, i);
+    }
+    return 0;
+}
+
